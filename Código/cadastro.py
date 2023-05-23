041e6f1f1ae2b1e7a0b6e74ca27ad7bc4935529a
leandroloffeu <112645165+leandroloffeu@users.noreply.github.com>
2023-05-23 15:58:48 -0300

    Update cadastro.py
    Classe animal

--- Código/cadastro.py
+++ Código/cadastro.py
@@ -2,6 +2,9 @@ class Animal:
     def __init__(self, tipo, idade, cor, porte, particularidade):
       self.tipo = tipo
       self.idade = idade
+      self.cor = cor
+      self.porte = porte
+
         
 
       
