802d920a5757e8365359da1024065a52e1e338a1
leandroloffeu <112645165+leandroloffeu@users.noreply.github.com>
2023-05-25 19:57:28 -0300

    Update and rename cadastro.py to animal.py

---
+++ Código/animal.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self, tipo, idade, cor, porte, particularidade):
+        self.tipo = tipo
+        self.idade = idade
+        self.cor = cor
+        self.porte = porte
+        self.particularidade = particularidade
+
